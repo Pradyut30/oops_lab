347641397cd60924fe0803c3d033fffd28ad7de0
Pradyut30 <121106850+Pradyut30@users.noreply.github.com>
2023-07-27 05:53:00 +0530

    Create 14(b).operator->.cpp

---
+++ 14(b).operator->.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+class loc {
+   public:
+   int a;
+   loc *operator->(){
+       return this;
+   }
+    
+};
+int main()
+{
+    loc ob;
+    ob->a=10;
+    cout<<ob.a<<" "<<ob->a;
+    return 0;
+}
